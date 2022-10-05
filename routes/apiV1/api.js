3142e58ecf525bcc8e987647f002dc6ea1f72752
Sebastian Morgado <sebaah89@gmail.com>
2022-10-05 12:43:38 -0300

    update.

--- routes/apiV1/api.js
+++ routes/apiV1/api.js
@@ -4,37 +4,7 @@ var router = express.Router();
 
 router.get('/stream/:streamName', function (req, res, next) {
 	//TODO: search streamer data using streamer name on database
-	res.send({
-		URL: `http://localhost:8000/live/${req.params.streamId}.flv`,
-		Name: 'test',
-		Followers: 0,
-		Views: 0,
-	});
-});
-
-router.get('/search/stream', function (req, res, next) {
-	//TODO: search streamer data using streamer name on database
-
-	//let streamerId = db.search(req.query.search, req.query.page);
-
-	/*
-	URL: `http://localhost:8000/live/${req.params.streamId}.flv`,
-
-	RedesSociales: ['test'],
-	Followers: 0,
-	ImagenCanal: 'test',
-	BannerOffline: 'test',
-	
-	Name: 'test', //stream data
-	AcercaDe: 'test', //stream data
-	Titulo: 'test', //stream data
-	Categoria: 'test', //stream data
-	Tiempo: 'test', //stream data
-	Idiomas: 'test', //stream data
-	Views: 0, //stream data
-	
-	*/
-	res.send({
+	res.send(JSON.stringify({
 		URL: `http://localhost:8000/live/${req.query.query}.flv`,
 		Name: 'test',
 		About: 'this is a test about description',
@@ -44,12 +14,97 @@ router.get('/search/stream', function (req, res, next) {
 		Category: 'csgo',
 		Followers: 0,
 		Views: 0,
-	});
+	}));
+});
+
+router.get('/search/stream', function (req, res, next) {
+	//TODO: search streamer data using streamer name on database
+
+	//let streamerId = db.search(req.query.search, req.query.page);
+
+	/*
+	sin directo:
+		title
+		followers
+		about
+
+	con directo:
+		titulo
+		juego
+		views
+		tags
+	*/
+
+	res.send(JSON.stringify(
+		[
+			{
+				Username: 'test',
+				Category: 'csgo',
+				Title: 'test stream xd',
+				Views: 0,
+				About: 'this is a test about description',
+				Tags: ['test1', 'vtuber', 'spain'],
+				Time: "2022-10-04T19:55:53.790Z",
+			},
+			{
+				Username: 'test2',
+				Followers: 0,
+				About: 'this is a test about description',
+			},
+			{
+				Username: 'test3',
+				Followers: 0,
+				About: 'this is a test about description',
+			},
+			{
+				Username: 'test4',
+				Followers: 0,
+				About: 'this is a test about description',
+			},
+			{
+				Username: 'test5',
+				Followers: 0,
+				About: 'this is a test about description',
+			},
+		]
+	));
 });
 
 router.get('/discover/stream', function (req, res, next) {
 	//let randomStreamers = db.search(req.query.page);
-	res.send("");
+	res.send(JSON.stringify(
+		[
+			{
+				Username: 'test',
+				Category: 'csgo',
+				Title: 'test stream xd',
+				Views: 0,
+				About: 'this is a test about description',
+				Tags: ['test1', 'vtuber', 'spain'],
+				Time: "2022-10-04T19:55:53.790Z",
+			},
+			{
+				Username: 'test2',
+				Followers: 0,
+				About: 'this is a test about description',
+			},
+			{
+				Username: 'test3',
+				Followers: 0,
+				About: 'this is a test about description',
+			},
+			{
+				Username: 'test4',
+				Followers: 0,
+				About: 'this is a test about description',
+			},
+			{
+				Username: 'test5',
+				Followers: 0,
+				About: 'this is a test about description',
+			},
+		]
+	));
 });
 
 
